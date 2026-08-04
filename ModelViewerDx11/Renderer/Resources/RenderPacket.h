efee86bebf5b96dc32932abc5bb9ba320efa3f88
SkyLake <Seobkim15@gmail.com>
2026-08-04 23:49:29 +0900

    Refactor: SortKey 64it로 변경 및 각 항목 비트 영역대로 조정
     - 설계 코멘트도 업데이트
     - 산술 버그가 발견되어 분리 커밋으로 다음 커밋에 수정 반영

--- ModelViewerDx11/Renderer/Resources/RenderPacket.h
+++ ModelViewerDx11/Renderer/Resources/RenderPacket.h
@@ -58,9 +58,9 @@ namespace renderer
             ASSERT((sampler != eSamplerType::SamplerCount && command.RenderState.SamplerBindingSlot != -1) || (sampler == eSamplerType::SamplerCount && command.RenderState.SamplerBindingSlot == -1),
                 "Sampler가 사용되지만 지정하지 않았거나, Sampler를 지정했지만 사용되지 않습니다. shader(%d), sampler(%d) bindingSlot(%d)", shader, sampler, command.RenderState.SamplerBindingSlot);
 
-            // MEMO: 32bit, 내림차순 정렬(값이 큰 순서로 렌더링)
+            // MEMO: 64bit, 내림차순 정렬(값이 큰 순서로 렌더링)
             // MEMO:  높은 쪽 <-----------> 낮은 쪽
-            // MEMO: 상위 비트 | 중간 비트 | 하위 비트
+            // MEMO: 상위 비트 (63-55) | 중간 비트 (54-20) | 하위 비트 (19-0)
 
             // MEMO: 추후에 작업하면서 필요에 따라 Bit 수, 순서 조정하면서 정답을 찾아가기.
 
@@ -69,7 +69,7 @@ namespace renderer
             // MEMO: 패스는 렌더타겟보다 상위여야 할지? 하위여야 할지? - 현재 프로젝트에서는 렌더타겟 == 패스이므로 패스는 무시.
             // MEMO: 머티리얼은 우선, 중간 비트를 사용한다. -> 그러나 아직 머티리얼 식별자가 없으므로 제외한다. 조만간 바로 작업 들어가야 함.
             // MEMO: 투명/불투명 여부도 중간 비트를 사용한다. 중간에서 가장 최상위로 둔다.
-            // MEMO: 셰이더는, 머티리얼보다 높은 쪽을 사용한다.
+            // MEMO: 셰이더는 우선 낮은 비트를 사용하되, 중간 비트로 올릴지 고민
             // 현재는 머티리얼당 셰이더 하나와 대응되어 의미 없지만 같은 셰이더를 공유하는 머티리얼이 있다면 대응이 될 수 있는 구조로 판단됨.
             // MEMO: 나머지 렌더 상태는 하위 비트를 쓴다. 현재 구조에 따라서 잘 안 바뀔 것 같은 것을 높은쪽에 둔다.
 
@@ -89,22 +89,22 @@ namespace renderer
             // TODO: 텍스처 해시도 동일한 텍스처를 쓰는 드로우콜을 뭉치면 좋을 것 같지만, 그렇게하면 해시가 아니라 다른 ID로 써야할 것 같다.
 
             // TODO: 설계 문서 대로 비트 위치는 구분해놓는 것이 깔끔할 것 같다. -> 후순위. 64비트로 바꿔야 할 수 있으므로 텍스처/머티리얼 식별자를 먼저 작업하고 대응한다.
-            uint32_t sortKey = 0;
+            uint64_t sortKey = 0;
             // MEMO: 상위 비트 영역
-            sortKey |= (RenderTargetPriority[static_cast<uint8_t>(command.RenderTargetType)] << 31);
+            sortKey |= (RenderTargetPriority[static_cast<uint8_t>(command.RenderTargetType)] << 63);
             // MEMO: 중간 비트 영역
             // MEMO: 내림자순이므로, 값이 반전되도록 해야 불투명을 먼저 그림
-            sortKey |= static_cast<uint8_t>(command.bTransparency == false) << 30;
+            sortKey |= static_cast<uint8_t>(command.bTransparency == false) << 54;
+            sortKey |= static_cast<uint8_t>(command.Material.TextureSerials[static_cast<uint8_t>(eTextureType::Diffuse)]) << 20;
             // MEMO: 하위 비트 영역
-            sortKey |= static_cast<uint8_t>(command.BufferUsage) << 29;
-            sortKey |= static_cast<uint8_t>(command.RenderState.DepthStencilUsage) << 28;
-            sortKey |= static_cast<uint8_t>(command.RenderState.bClearDepthStencilBuffer) << 27;
-            sortKey |= static_cast<uint8_t>(command.RenderState.ShaderType) << 26;
-            sortKey |= static_cast<uint8_t>(command.RenderState.UseShadowMapUsage) << 25;
-            sortKey |= static_cast<uint8_t>(command.RenderState.ShaderType) << 21;
-            sortKey |= static_cast<uint8_t>(command.VertexFormat) << 18;
-            sortKey |= static_cast<uint8_t>(command.RenderState.RasterType) << 14;
-            sortKey |= static_cast<uint8_t>(command.RenderState.TopologyType) << 10;
+            sortKey |= static_cast<uint8_t>(command.BufferUsage) << 18;
+            sortKey |= static_cast<uint8_t>(command.RenderState.DepthStencilUsage) << 17;
+            sortKey |= static_cast<uint8_t>(command.RenderState.bClearDepthStencilBuffer) << 16;
+            sortKey |= static_cast<uint8_t>(command.RenderState.UseShadowMapUsage) << 15;
+            sortKey |= static_cast<uint8_t>(command.RenderState.ShaderType) << 11;
+            sortKey |= static_cast<uint8_t>(command.VertexFormat) << 8;
+            sortKey |= static_cast<uint8_t>(command.RenderState.RasterType) << 4;
+            sortKey |= static_cast<uint8_t>(command.RenderState.TopologyType) << 0;
 
             command.SortKey = sortKey;
 
@@ -124,7 +124,7 @@ namespace renderer
         eRenderTarget RenderTargetType;
         XMMATRIX MatWorld;
         RenderState RenderState;
-        uint32_t SortKey;
+        uint64_t SortKey;
     };
 
     // MEMO: RenderPacket을 사용하지 않고, 캐시가 필요한 상태값들만 명확하게 모아서 처리한다.
