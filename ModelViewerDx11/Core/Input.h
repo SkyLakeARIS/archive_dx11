88f8fdfe47bf455173168ffa0aa27183427670e2
SkyLake <Seobkim15@gmail.com>
2026-08-22 20:52:33 +0900

    Chore: 미사용 코드(MyInput) 제거

--- ModelViewerDx11/Core/Input.h
+++ ModelViewerDx11/Core/Input.h
@@ -55,48 +55,4 @@ namespace core
 
         UINT32                  mControlState;
     };
-
-
-    /*
-     * 
-     * 직접 만든 keyboard/mouse Input 클래스
-     *
-     * 현재는 DirectInput과 다른점이 안보이나, 프로그램이 커지기 시작해야
-     * 성능 문제가 보이지 않을까 예상.
-     */
-    class MyInput final
-    {
-    public:
-        MyInput();
-        ~MyInput();
-
-        HRESULT     Initialize(HWND* const hwnd, int screenWidth, int screenHeight);
-        void        Release();
-
-        void        UpdateWindowSize(int newWidth, int newHeight);
-
-        bool        UpdateMouseInput(const MSG& msg);
-        bool        UpdateKeyboardInput(const MSG& msg);
-
-        void        SetControlMode(uint32 flag);
-
-        void        GetMousePosition(int& mouseX, int& mouseY) const;
-        void        GetMouseDeltaPosition(int& deltaX, int& deltaY) const;
-        void        GetKeyboardPressed(bool* keyboardState, int* size) const;
-        uint32      GetControlMode() const;
-
-    private:
-        HWND* mWnd;
-        bool                    mKeyboardState[256];
-
-        int                     mMouseX;
-        int                     mMouseY;
-        int                     mMouseDeltaX;
-        int                     mMouseDeltaY;
-
-        int                     mScreenWidth;
-        int                     mScreenHeight;
-
-        UINT32                  mControlState;
-    };
 }
